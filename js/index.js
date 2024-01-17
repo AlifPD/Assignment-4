1221bd7bf706784d0b6e52a2ec11cc848b32b9c5
AlifPD <muhammadalifputradafi@gmail.com>
2024-01-18 05:12:14 +0700

    Integrate API

---
+++ js/index.js
@@ -0,0 +1,50 @@
+const locInput = document.getElementById("locInput");
+const locBtn = document.getElementById("locBtn");
+const locForm = document.getElementById("locForm");
+
+const activeCasesTv = document.getElementById("activeCasesTv");
+const newCasesTv = document.getElementById("newCasesTv");
+const recoveredCasesTv = document.getElementById("recoveredCasesTv");
+const totalCasesTv = document.getElementById("totalCasesTv");
+const totalDeathsTv = document.getElementById("totalDeathsTv");
+const totalTestsTv = document.getElementById("totalTestsTv");
+
+const xhr = new XMLHttpRequest();
+let data = {};
+
+// locBtn.addEventListener("click", function(){
+//     getLocData();
+//     locInput.value = ""
+// });
+
+locForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    getLocData();
+    this.reset();
+})
+
+xhr.addEventListener("readystatechange", function(){
+    if(this.readyState === this.DONE){
+        data = JSON.parse(this.response);
+        console.log(data);
+        showData();
+    }
+})
+
+function getLocData(){
+    xhr.open('GET', `https://covid-193.p.rapidapi.com/statistics?country=${locInput.value}`);
+    xhr.setRequestHeader('X-RapidAPI-Key', '850ecbb5cbmsh5829d0c239efc33p14be0cjsn555cafe0330e');
+    xhr.setRequestHeader('X-RapidAPI-Host', 'covid-193.p.rapidapi.com');
+
+    xhr.send();
+}
+
+function showData(){
+    activeCasesTv.innerHTML = data.response[0].cases.active;
+    newCasesTv.innerHTML = data.response[0].cases.new;
+    recoveredCasesTv.innerHTML = data.response[0].cases.recovered;
+    totalCasesTv.innerHTML = data.response[0].cases.total;
+    totalDeathsTv.innerHTML = data.response[0].deaths.total;
+    totalTestsTv.innerHTML = data.response[0].tests.total;
+}
