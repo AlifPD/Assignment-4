037212a12324d6b7998362d0a1799a683698c001
AlifPD <muhammadalifputradafi@gmail.com>
2024-01-18 10:18:30 +0700

    Change API access to using fetch

--- js/index.js
+++ js/index.js
@@ -9,38 +9,34 @@ const totalCasesTv = document.getElementById("totalCasesTv");
 const totalDeathsTv = document.getElementById("totalDeathsTv");
 const totalTestsTv = document.getElementById("totalTestsTv");
 
-const xhr = new XMLHttpRequest();
-let data = {};
-
-// locBtn.addEventListener("click", function(){
-//     getLocData();
-//     locInput.value = ""
-// });
+// const xhr = new XMLHttpRequest();
 
 locForm.addEventListener("submit", function(event){
     event.preventDefault();
 
     getLocData();
+
     this.reset();
 })
 
-xhr.addEventListener("readystatechange", function(){
-    if(this.readyState === this.DONE){
-        data = JSON.parse(this.response);
-        console.log(data);
-        showData();
-    }
-})
 
-function getLocData(){
-    xhr.open('GET', `https://covid-193.p.rapidapi.com/statistics?country=${locInput.value}`);
-    xhr.setRequestHeader('X-RapidAPI-Key', '850ecbb5cbmsh5829d0c239efc33p14be0cjsn555cafe0330e');
-    xhr.setRequestHeader('X-RapidAPI-Host', 'covid-193.p.rapidapi.com');
-
-    xhr.send();
+async function getLocData(){
+    let locDataResponse = await fetch('https://covid-193.p.rapidapi.com/statistics?country=indonesia', {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '850ecbb5cbmsh5829d0c239efc33p14be0cjsn555cafe0330e',
+            'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
+        }
+    });
+    
+    let locDataResponseParsed = await locDataResponse.json();
+    
+    showData(locDataResponseParsed);
+    
 }
 
-function showData(){
+function showData(data){
+    console.log(data);
     activeCasesTv.innerHTML = data.response[0].cases.active;
     newCasesTv.innerHTML = data.response[0].cases.new;
     recoveredCasesTv.innerHTML = data.response[0].cases.recovered;
@@ -48,3 +44,19 @@ function showData(){
     totalDeathsTv.innerHTML = data.response[0].deaths.total;
     totalTestsTv.innerHTML = data.response[0].tests.total;
 }
+
+// xhr.addEventListener("readystatechange", function(){
+//     if(this.readyState === this.DONE){
+//         data = JSON.parse(this.response);
+//         console.log(data);
+//         showData();
+//     }
+// })
+
+// function getLocData(){
+//     xhr.open('GET', `https://covid-193.p.rapidapi.com/statistics?country=${locInput.value}`);
+//     xhr.setRequestHeader('X-RapidAPI-Key', '850ecbb5cbmsh5829d0c239efc33p14be0cjsn555cafe0330e');
+//     xhr.setRequestHeader('X-RapidAPI-Host', 'covid-193.p.rapidapi.com');
+
+//     xhr.send();
+// }
